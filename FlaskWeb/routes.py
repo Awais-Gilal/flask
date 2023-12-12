f3532b7e596b721a330709eb519f7d65da3b7cc7
Awais-Gilal <awaisgilal4dadu@gmail.com>
2023-12-11 19:14:22 -0800

    added form in templates and other

--- FlaskWeb/routes.py
+++ FlaskWeb/routes.py
@@ -1,6 +1,7 @@
 from FlaskWeb import app, db
 from flask import render_template
 from FlaskWeb.modules import Posts, Messages
+from FlaskWeb.forms import PostForm, MessageForm
 
 
 @app.route("/")
@@ -9,24 +10,6 @@ def home_page():
   with app.app_context():
     posts = Posts.query.all()
   return render_template("index.html", posts=posts)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -42,3 +25,9 @@ def contact_page():
 @app.route("/post")
 def post_page():
   return render_template("postPage.html")
+
+
+@app.route("/postform" , methods=["GET", "POST"])
+def post_form_page():
+  form = PostForm
+  return render_template("postForm.html", form=form)
